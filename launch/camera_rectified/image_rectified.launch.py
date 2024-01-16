b4725fecf7deb1cc43affc961296d5b88a78cd7e
zymouse <zymouse@pixmoving.net>
2024-01-16 15:10:02 +0800

    feat：更新

--- launch/camera_rectified/image_rectified.launch.py
+++ launch/camera_rectified/image_rectified.launch.py
@@ -3,18 +3,43 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
 
 
 def generate_launch_description():
-    node = Node(
-      package='output_rectified_image',
-      executable='output_rectified_image_node',
-      name='output_rectified_image_node',
-      remappings=[
-        ('input_image_raw', '/sensing/camera/traffic_light/image_raw'),
-        ('input_camera_info', '/sensing/camera/traffic_light/camera_info'),
+    launch_arguments = []
 
-        ('output_image_raw', '/sensing/camera/traffic_light/rectified/image_raw'),
-        ('output_camera_info', '/sensing/camera/traffic_light/rectified/camera_info')],
+    def add_launch_arg(name: str, default_value=None):
+        launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
+
+    add_launch_arg("input_image_raw", "/sensing/camera/top/gmsl/image_raw")
+    add_launch_arg("input_camera_info", "/sensing/camera/top/gmsl/camera_info")
+    add_launch_arg("output_image_raw", "/sensing/camera/top/gmsl/rectified")
+    add_launch_arg("output_camera_info", "/rectified/camera_info")
+    # node = Node(
+    #   package='output_rectified_image_py',
+    #   executable='output_rectified_image_py_node',
+    #   name='output_rectified_image_py_node',
+    #   remappings=[
+    #     ('input_image_raw', LaunchConfiguration("input_image_raw")),
+    #     ('input_camera_info', LaunchConfiguration("input_camera_info")),
+
+    #     ('output_image_raw', LaunchConfiguration("output_image_raw")),
+    #     ('output_camera_info', LaunchConfiguration("output_camera_info"))],
+    #   output='screen')
+    
+    node = Node(
+      package='output_rectified_image_cpp',
+      executable='output_rectified_image_cpp_node',
+      name='output_rectified_image_cpp_node',
+      remappings=[
+        ('image_raw',  LaunchConfiguration("input_image_raw")),
+        ('camera_info', LaunchConfiguration("input_camera_info")),
+
+        ('rectified/image_raw', LaunchConfiguration("output_image_raw")),
+        ('rectified/camera_info', LaunchConfiguration("output_camera_info"))],
       output='screen')
-    return LaunchDescription([node])
+  
+    return LaunchDescription(launch_arguments+[node,])
--- launch/cameratag_viz/viz.launch.py
+++ launch/cameratag_viz/viz.launch.py
@@ -1,21 +1,36 @@
 import launch
 from launch_ros.actions import ComposableNodeContainer
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.descriptions import ComposableNode
+from launch.actions import DeclareLaunchArgument
+
 
 def generate_launch_description():
-    composable_node = ComposableNode(name='viz', package='apriltag_viz', plugin='AprilVizNode',
-                        remappings=[("image", "/sensing/camera/traffic_light/rectified/image_raw"),
-                                    ("detections", "/sensor_kit/sensor_kit_base_link/traffic_light_left_camera/camera_link/apriltag/detection_array")
-                                #     ("detections", "/apriltag/detection_array")
-                                    ],)
+    launch_arguments = []
+
+    def add_launch_arg(name: str, default_value=None):
+        launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
+
+    add_launch_arg("image_topic", "/sensing/camera/front/rectified/image_raw")
+    add_launch_arg("detections", "/sensor_kit/sensor_kit_base_link/top/gmsl/apriltag/detection_array")
+
+    composable_node = ComposableNode(
+                        name='viz', 
+                        package='apriltag_viz', 
+                        plugin='AprilVizNode',
+                        remappings=[("image", LaunchConfiguration("image_topic")),
+                                    ("detections", LaunchConfiguration("detections"))
+                                    ],
+                        )
     container = ComposableNodeContainer(
             name='viz_container',
             namespace='apriltag',
             package='rclcpp_components',
             executable='component_container',
             composable_node_descriptions=[composable_node],
-            remappings=[("/apriltag/image", "/sensing/camera/traffic_light/image_raw/decompressed")],
+            remappings=[("/apriltag/image", "/sensing/camera/front/image_raw/decompressed")],
             output='screen'
     )
 
-    return launch.LaunchDescription([container])
+    return launch.LaunchDescription(launch_arguments+[container])
